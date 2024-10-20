9823d0c3672be3abd83263c64eb8a748fc4a87bf
Adam <adamgruda1312@gmail.com>
2024-10-20 18:19:02 +0200

    protein example visualization

--- viz4go_frontend/lib/home_screen.dart
+++ viz4go_frontend/lib/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_3d_controller/flutter_3d_controller.dart';
 import 'package:viz4go_frontend/models/node.dart';
 import 'package:viz4go_frontend/widgets/line_painter.dart';
 import 'package:viz4go_frontend/widgets/menu.dart';
@@ -31,6 +32,7 @@ class _HomeScreenState extends State<HomeScreen> {
   bool isCsv = false;
   List<String> _hoveredNodes =
       []; // Nowa zmienna do śledzenia najechanego węzła
+  Flutter3DController controller = Flutter3DController();
 
   void _generateGraphFromTextField(List<dynamic> newItems) {
     // TO DO umoliwić wrzucenie tylko jednego pliku .csv, albbo dwóch
@@ -232,11 +234,48 @@ class _HomeScreenState extends State<HomeScreen> {
                     width: 200,
                     height: 200,
                     decoration: BoxDecoration(
-                      color: Colors.blueGrey[400],
+                      color: Colors.blueGrey[700],
                       borderRadius: BorderRadius.circular(10),
                     ),
                     child: Center(
-                      child: Text(_protein),
+                      child: Column(
+                        children: [
+                          Text(_protein,
+                              style: const TextStyle(color: Colors.white)),
+                          Flexible(
+                              flex: 1,
+                              child: Flutter3DViewer(
+                                //If you pass 'true' the flutter_3d_controller will add gesture interceptor layer
+                                //to prevent gesture recognizers from malfunctioning on iOS and some Android devices.
+                                //the default value is true
+                                activeGestureInterceptor: true,
+                                //If you don't pass progressBarColor, the color of defaultLoadingProgressBar will be grey.
+                                //You can set your custom color or use [Colors.transparent] for hiding loadingProgressBar.
+                                progressBarColor: Colors.orange,
+                                //You can disable viewer touch response by setting 'enableTouch' to 'false'
+                                enableTouch: true,
+                                //This callBack will return the loading progress value between 0 and 1.0
+                                onProgress: (double progressValue) {
+                                  debugPrint(
+                                      'model loading progress : $progressValue');
+                                },
+                                //This callBack will call after model loaded successfully and will return model address
+                                onLoad: (String modelAddress) {
+                                  debugPrint('model loaded : $modelAddress');
+                                },
+                                //this callBack will call when model failed to load and will return failure error
+                                onError: (String error) {
+                                  debugPrint('model failed to load : $error');
+                                },
+                                //You can have full control of 3d model animations, textures and camera
+                                controller: controller,
+                                src:
+                                    'assets/example.gltf', //3D model with different animations
+                                //src 'assets/sheen_chair.glb', //3D model with different textures
+                                //'https://modelviewer.dev/shared-assets/models/Astronaut.glb', // 3D model from URL
+                              )),
+                        ],
+                      ),
                     ),
                   ))
               : const Align(),
